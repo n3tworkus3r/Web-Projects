f011e43c9c05ae19f752f1f7ca6f7b819c665945
n3tworkus3r <network.us3r@gmail.com>
2022-06-24 17:02:16 +0300

    Nav and lib update

--- Lynx/main/views.py
+++ Lynx/main/views.py
@@ -5,4 +5,7 @@ def login(request):
 
 def library(request):
   track_list = ['1', '2', '3', '4']
-  return render(request, 'main/library.html',context={'track_list': track_list})
+  return render(request, 'main/library.html', context={'track_list': track_list})
+
+def nav(request):
+  return render(request, 'main/nav.html')
